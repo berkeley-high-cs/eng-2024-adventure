e721af47795afe363965ba77d9e1ec56224a9aa3
RoomDoom01 <85605670+RoomDoom01@users.noreply.github.com>
2024-05-26 20:48:00 +0000

    recursive passage and room loop

--- AdventureGame/src/net/berkeley/students/eng2024/Main.java
+++ AdventureGame/src/net/berkeley/students/eng2024/Main.java
@@ -3,6 +3,7 @@ package net.berkeley.students.eng2024;
 class Main {
 
     public static void main(String[] args) {
+        System.out.println(Save.toCSV());
         new AdventureGame();
     }
 }
--- AdventureGame/src/net/berkeley/students/eng2024/Save.java
+++ AdventureGame/src/net/berkeley/students/eng2024/Save.java
@@ -4,72 +4,82 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
 import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.Path;
+import java.util.Arrays;
 
-static class Save{
-    public static AdventureGame download(String path){
-        BufferedReader br;
-        try{
-            br=new BufferedReader(Path.of(path));
-        }
-        catch(IOException e){
-            System.out.println("The file you are trying to load a save state from does not exist.");
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-    }
+public class Save{
+    // public static AdventureGame download(String path){
+    //     BufferedReader br;
+    //     try{
+    //         br=new BufferedReader(Path.of(path));
+    //     }
+    //     catch(IOException e){
+    //         System.out.println("The file you are trying to load a save state from does not exist.");
+    //     }
+    //     catch(Exception e){
+    //         e.printStackTrace();
+    //     }
+    // }
     public static void upload(String path){
         FileWriter fw;
         try{
             fw=new FileWriter(path);
         }
         catch(IOException io){
-            if(System.Console.readLine("This file does not exist, create it? (y/n)").equals("y")){
-                File f=new File(path);
-                f.createNewFile();
-                fw=new FileWriter(f);
+            if(System.console().readLine("This file does not exist, create it? (y/n)").equals("y")){
+                try{
+                    File f=new File(path);
+                    f.createNewFile();
+                    fw=new FileWriter(f);
+                    fw.write(toCSV());
+                    fw.close();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
             }
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        fw.write(toCSV());
     }
     public static String toCSV(){
         GameMap gm=AdventureGame.map;
         Player p=AdventureGame.player;
-        Room[] rooms=gm.getRooms();
-        Passage[] passages=gm.getPassages();
-        Item[] inventory=p.getItems().toArray();
-        String roomString=roomsToCSV(rooms);
-        String passageString=passagesToCSV(passages);
+        Room[] rooms=gm.rooms();
+        Passage[] passages=gm.passages();
+        List<Item> inventory=p.items();
+        String roomString=roomsToCSV(Arrays.asList(rooms));
+        String passageString=passagesToCSV(Arrays.asList(passages));
         String inventoryString=inventoryToCSV(inventory);
-        String visited=roomsToCSV(p.getVisitedRooms());
+        String visited=roomsToCSV(p.visitedRooms());
         String passagesTaken=passagesToCSV(p.getPassages());
+        return "{"+roomString+"},{"+passageString+"},{"+inventoryString+"},{"+visited+"},{"+passagesTaken+"}";
     }
-    public static String roomsToCSV(Room[] rooms){
-        StringBuilder sb=foo;
+    public static String roomsToCSV(List<Room> rooms){
+        StringBuilder sb=new StringBuilder();
         for(Room r: rooms){
             sb.append("{"+roomToCSV(r)+"}");
         }
         return sb.toString();
     }
     public static String roomToCSV(Room room){
-        StringBuilder foo;
-        foo.append(room.getName()+";"+room.getDescription());
-        foo.append("pas{"+passagesToCSV(room.getPassages())+"}");
-        foo.append("ent{"+entitiesToCSV(room.getEntities())+"}");
+        StringBuilder foo=new StringBuilder();
+        foo.append(room.codeName()+";"+room.description());
+        foo.append("pas{"+passagesToCSV(room.passages())+"}");
+        foo.append("ent{"+entitiesToCSV(room.entities())+"}");
         return foo.toString();
     }
-    public static String passagesToCSV(Passage[] passes){
-        StringBuilder sb;
+    public static String passagesToCSV(List<Passage> passes){
+        StringBuilder sb=new StringBuilder();
         for(Passage p:passes){
             sb.append("{"+passageToCSV(p)+"}");
         }
         return sb.toString();
     }
     public static String passageToCSV(Passage pass){
-        StringBuilder foo;
+        StringBuilder foo=new StringBuilder();
         foo.append(pass.getName()+"{");
         for(String n:pass.getAllNames()){
             foo.append(n+",");
@@ -78,24 +88,28 @@ static class Save{
         foo.append("}"+pass.getMovementDescription()+"{"+roomToCSV(pass.getRoom1())+"},{"+roomToCSV(pass.getRoom2())+"}"+pass.isAccessible());
         return foo.toString();
     }
-    public static String inventoryToCSV(Item[] inv){
-        StringBuilder sb;
+    public static String inventoryToCSV(List<Item> inv){
+        StringBuilder sb=new StringBuilder();
         for(Item i:inv){
             sb.append("{"+itemToCSV(i)+"}");
         }
         return sb.toString();
     }
     public static String itemToCSV(Item i){
-        StringBuilder foo;
+        StringBuilder foo=new StringBuilder();
+
+        return foo.toString();
         
     }
-    public static String entitiesToCSV(Entity[] entities){
+    public static String entitiesToCSV(List<Entity> entities){
         StringBuilder sb;
         for(Entity e:entities){
             sb.append("{"+entityToCSV(i)+"}");
         }
     }
     public static String entityToCSV(Entity e){
+        StringBuilder foo=new StringBuilder();
 
+        return foo.toString();
     }
 }
